5675cfd2dc35d6bb61df00d50b5d84ac4660d7af
seyhomer <hsnkeser@gmail.com>
2018-10-19 11:04:37 +0300

    Add files via upload

---
+++ MDK-ARM/RTE/_adc1-demo/RTE_Components.h
@@ -0,0 +1,20 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'adc1-demo' 
+ * Target:  'adc1-demo' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f4xx.h"
+
+
+#endif /* RTE_COMPONENTS_H */
